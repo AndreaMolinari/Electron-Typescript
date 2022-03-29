1f12ebfdb4bf3598b8eb1d3c67bd68e05936d0a9
Andrea Molinari <55259968+AndreaMolinari@users.noreply.github.com>
2022-03-29 21:41:13 +0200

    feat: :construction_worker: electron + typescript + react

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+
+const App: React.FC = () => {
+  return <>Ciao</>
+}
+
+export default App
+
+ReactDOM.render(<App />, document.body);
